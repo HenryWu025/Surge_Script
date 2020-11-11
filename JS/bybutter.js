224132b3648f317e0449b528e3f0c71c3ff6166f
HenryWu025 <74251234+HenryWu025@users.noreply.github.com>
2020-11-11 11:08:32 +0800

    Update bybutter.js

--- JS/bybutter.js
+++ JS/bybutter.js
@@ -1,2 +1,20 @@
-body = $response.body.replace(/\"ownership\":membership/g, "\"ownership\":free").replace(/\"enable\":false/g, "\"enable\":true").replace(/\"memberships\":\[\]/g, "\"memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"temporary\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]").replace(/\"trial\":false/g, "\"trial\":true").replace(/\"colorful\":false/g, "\"colorful\":true").replace(/\"masking\":false/g, "\"masking\":true").replace(/"remark":"[^""]+"/g, '\"remark\":\"使用期限：永久"').replace(/\"ownership\":builtin/g, "\"ownership\":free").replace(/\"ownership\":promotion/g, "\"ownership\":free").replace(/\"usageType\":view/g, "\"usageType\":unlimited").replace(/\"ownership\":membership/g, "\"ownership\":free")
-$done({body});
+const me = "/v4/users/me";
+const shop = "/v4/shop";
+const search = "/v4/search/products";
+var url = $request.url;
+
+if ($request.method == 'OPTIONS'){
+ $done({});
+}else{
+var body = $response.body;
+ if (url.indexOf(me) != -1) {
+  body = body.replace(/memberships":\\[\\]/g, 'memberships":[{"endAt":2493043200,"id":"1","name":"普通会员","ownership":"temporary","startAt":1587425677,"usageType":"unlimited"}]');
+}
+if (url.indexOf(shop) != -1) {
+  body = body.replace(/ownership":"\w+"/g, 'ownership":"free"').replace(/usageType":"\w+"/g, 'usageType":"unlimited"').replace(/remark":"[^"]+"/g, 'remark":"使用期限：永久"');
+}
+if (url.indexOf(search) != -1) {
+ body = body.replace(/"enable":false/g, '"enable":true').replace(/"style":"membership_only"/, '"style":"none"');
+}
+$done({ body });
+}
